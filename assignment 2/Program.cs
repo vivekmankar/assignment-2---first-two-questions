5e03b45affacfa10b5bb2a82037fe03115b5bb29
Vivek Mankar <vivekmankar@usf.edu>
2020-10-04 18:52:40 -0400

    assignment 2

--- assignment 2/Program.cs
+++ assignment 2/Program.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿//using System.IO;
+//using System;
 
-namespace assignment_2
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Hello, World!");
+//        PrintPatternLinearComplexity(5);
+//    }
+
+//    public static void PrintPatternLinearComplexity(int n)
+//    {
+
+//        for (int i = 1; i <= n; i++)
+//        {
+//            string stars = new String('*', i);
+//            Console.WriteLine(stars + "\n");
+//            Console.ReadKey();
+
+//        }
+
+//    }
+//}
+
+//==================================================================================
+using System.IO;
+using System;
+
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+
+        int[] arr = new int[] { 1, 3, 5, 4, 7 };
+
+        Console.WriteLine(LongestSubSeq(arr));
+        Console.ReadKey();
+    }
+
+    public static int LongestSubSeq(int[] nums)
+    {
+        int length = nums.Length;
+        int longestSubSeq = 1;
+        int prevLongestSubSeq = 1;
+        for (int i = 0; i < (length - 1); i++)
         {
-            Console.WriteLine("Hello World!");
+            //update count if conditions validates
+            if (nums[i] < nums[i + 1])
+            {
+                longestSubSeq++;
+            }
+            else
+            {
+                prevLongestSubSeq = longestSubSeq;
+                longestSubSeq = 1;
+            }
         }
+        //return longestSubSequence
+        return (longestSubSeq > prevLongestSubSeq) ? longestSubSeq : prevLongestSubSeq;
+
     }
 }
