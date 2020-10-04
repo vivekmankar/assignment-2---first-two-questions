c8598ebaff57ab6d48e56da6818e9cb61e0287fb
Vivek Mankar <vivekmankar@usf.edu>
2020-10-04 18:51:31 -0400

    Add project files.

---
+++ assignment 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
